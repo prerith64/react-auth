26369bf148280d83b929166d4a31099602da4b40
Prerith_shetty <123531490+prerith64@users.noreply.github.com>
2025-02-13 17:16:04 +0530

    Update index.js fornt

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express()
 dotenv.config()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(cors({origin:`${process.env.FRONT_END_URI}`,credentials:true}))
+app.use(cors({origin:'http://localhost:5173',credentials:true}))
 app.use(cookieParser())
 app.options("*", cors());
 
