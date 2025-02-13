981e1e433e99a71ff0f15881e8f955ef1961fd46
Prerith_shetty <123531490+prerith64@users.noreply.github.com>
2025-02-13 17:23:20 +0530

    Update index.js frontend

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express()
 dotenv.config()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(cors({origin:'http://localhost:5173',credentials:true}))
+app.use(cors({origin:'https://react-auth-4929.vercel.app',credentials:true}))
 app.use(cookieParser())
 app.options("*", cors());
 
